3e92f83d46396b25c4ad45a6afe3b99477a6db42
Elias Koivuporras <eppu2002@gmail.com>
2022-01-28 22:25:44 +0200

    passing props to child component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import './App.css';
-import { useState } from 'react';
-import React from 'react';
+//import { useState } from 'react';
+import React, { Component } from 'react';
 
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count : 0
+    }
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={() => {this.setState({count: this.state.count + 1})}}>Paina</button>
+        <div>{this.state.count}</div>
+        <Display count={this.state.count} />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+/*
 export default function App() {
+  constructor(props) {
+    super(props);
+  }
   const [count, setCount] = useState(0);
 
   const handleClick = () => {
@@ -12,7 +40,21 @@ export default function App() {
     <div>
       <button onClick={handleClick}>Paina</button>
       <div>{count}</div>
+      <Display />
     </div>
   );
+}
+*/
+
+class Display extends Component {
+  
+  render() {
+    return (
+      <div>
+        <h3>class component</h3>
+        <p>count: {this.props.count}</p>
+      </div>
+    );
+  }
 }
 

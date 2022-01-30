238565bd1ae5951498f2674a28d83c60e94faafd
Elias Koivuporras <eppu2002@gmail.com>
2022-01-30 15:20:50 +0200

    eka toimiva versio taskien lisäykseen

--- src/App.js
+++ src/App.js
@@ -4,19 +4,61 @@ import React, { Component } from 'react';
 import Display from './components/Display';
 
 
+
+const Test = (props) => {
+  return (
+  <div>
+    <h1>{props.nimi}</h1>
+  </div>
+  );
+}
+
+
 export class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      count : 0
+      count : 0,
+      nimi : "",
+      input: "",
+      items: []
     }
+    this.handlaaMuutos=this.handlaaMuutos.bind(this);
+    this.addText=this.addText.bind(this);
   }
+  componentDidMount() {
+    this.setState({
+      nimi: "Add Task v1"
+    })
+  }
+  handlaaMuutos = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+  addText = () => {
+    this.setState(previousState => ({
+      items: [...previousState.items, this.state.input]
+    }));
+   
+  }
+
   render() {
+    const listItems = this.state.items.map((item) =>
+      <li>{item}</li>
+    );
     return (
-      <div>
-        <button onClick={() => {this.setState({count: this.state.count + 1})}}>Paina</button>
-        <div>{this.state.count}</div>
-        <Display count={this.state.count} />
+      <div className='App'>
+        
+        <Test nimi={this.state.nimi}/>
+
+        <div class="container">
+          <input type="text" placeholder="Lisää tekstiä" onChange={this.handlaaMuutos}></input>
+          <button onClick={this.addText}>Lisää</button>
+          <div>
+            <ul>{listItems}</ul>
+          </div>
+        </div>
       </div>
     );
   }
@@ -24,28 +66,5 @@ export class App extends Component {
 
 export default App;
 
-
-
-
-
-/*
-export default function App() {
-  constructor(props) {
-    super(props);
-  }
-  const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount(count +1);
-  }
-  return (
-    <div>
-      <button onClick={handleClick}>Paina</button>
-      <div>{count}</div>
-      <Display />
-    </div>
-  );
-}
-*/
 
 
